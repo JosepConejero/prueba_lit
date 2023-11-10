a37c5b0ccb15a4e510f0fb28558ec209e1aa15a7
JosepConejero <bermeu@gmail.com>
2023-11-10 10:53:10 +0100

    Added media query to otro-componente

--- components/otro-componente.js
+++ components/otro-componente.js
@@ -11,16 +11,31 @@ export class OtroComponente extends LitElement {
   }
 
   static styles = css`
+  @media all and (max-width: 600px){
+    div{
+        width:100%;
+        height:auto;
+        margin-left:20px;
+        background-color: green;
+        border: 5px black solid;
+    }
+}
   p {
     margin: 0;
     background-color: red;
     padding: 0.5em;
+  }
+  p:hover {
+    background-color: yellow;
+    margin: 5px;
+    padding: 2em;
   }
 `;
 
   render() {
     return html`
     <p>MÁS COSAS</p>
+    <div><span>PRUEBA PARA EL MEDIA QUERY</span></div>
     `;
   }
 }
