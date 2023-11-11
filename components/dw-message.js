0d6971b23f7994824c1444834b6e28d920d6d726
JosepConejero <bermeu@gmail.com>
2023-11-11 23:00:12 +0100

    Transformed dw-message.js in a card

--- components/dw-message.js
+++ components/dw-message.js
@@ -1,21 +1,39 @@
-import {LitElement, html} from 'lit';
+import { LitElement, css, html } from "lit";
 
 export class DwMessage extends LitElement {
   static properties = {
     version: {},
   };
 
+  static styles = css`
+    .fondo {
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      width: 500px;
+      height: 300px;
+      background-color: lightgrey;
+    }
+    .tarjeta {
+      width: 470px;
+      height: 270px;
+      background-color: red;
+      border: 3px solid black;
+      border-radius: 20px;
+    }
+  `;
+
   constructor() {
     super();
-    this.version = 'FINISHING';
+    this.version = "FINISHING";
   }
 
   render() {
     return html`
-    <p>TAMBIÉN VA ESTO, ES FLIPANTE</p>
-    <p>VERSIÓN PACABÁ ${this.version}</p>
-       
+      <div class="fondo">
+        <div class="tarjeta"></div>
+      </div>
     `;
   }
 }
-customElements.define('dw-message', DwMessage);
+customElements.define("dw-message", DwMessage);
--- components/form-input.js
+++ components/form-input.js
@@ -1,6 +1,9 @@
 import { html, css, LitElement } from "lit";
 //import "form-input.css";
 import { devuelveNum, mas_css } from "./prueba";
+import "./dw-message";
+
+const { log } = console;
 
 export class FormInput extends LitElement {
   static properties = {
@@ -31,6 +34,12 @@ export class FormInput extends LitElement {
     this.variab3 = devuelveNum(this.variab1);
   }
 
+  decrementaBorde() {
+    this.variab1 -= 1;
+    this.variab2 = "";
+    this.variab3 = 0;
+  }
+
   constructor() {
     super();
     this.variab1 = 23;
@@ -38,11 +47,15 @@ export class FormInput extends LitElement {
   }
 
   render() {
-    console.log("pasa por aquí");
+    log("pasa por aquí");
+
     return html`
       <p class="parr" @click=${this.aumentarBorde}>casundena ${this.variab2}</p>
+      <button @click=${this.aumentarBorde}>AUMENTA</button>
+      <button @click=${this.decrementaBorde}>DECREMENTA</button>
       <div class="externo">PRUEBA RECUADRADA ${this.variab1}</div>
       <div class="externo">${this.variab3}</div>
+      <dw-message></dw-message>
     `;
   }
 }
