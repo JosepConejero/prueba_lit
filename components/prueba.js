c733191c243132993e7f33f62f14d642134609c8
JosepConejero <bermeu@gmail.com>
2023-11-13 21:07:41 +0100

    Added ...

--- components/prueba.js
+++ components/prueba.js
@@ -3,6 +3,9 @@ import { css } from "lit";
 export const devuelveNum = (num) => num * 2;
 
 export const mas_css = css`
+  * {
+    outline: 1px solid red;
+  }
   :host {
     display: block;
     border: 1px solid black;
